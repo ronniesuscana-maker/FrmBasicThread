145142bb9bdfd2a227a576aaaeac2af9a7e1d155
ronniesuscana-maker <ronniesuscana@gmail.com>
2025-09-18 18:43:23 +0800

    Add project files.

---
+++ FrmBasicThread/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace FrmBasicThread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Run = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(231, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(369, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "-Before starting thread-";
+            // 
+            // Run
+            // 
+            this.Run.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Run.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Run.Location = new System.Drawing.Point(356, 282);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(119, 47);
+            this.Run.TabIndex = 1;
+            this.Run.Text = "Run";
+            this.Run.UseVisualStyleBackColor = false;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FrmBasicThread.Properties.Resources._123;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Run);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Run;
+    }
+}
+
